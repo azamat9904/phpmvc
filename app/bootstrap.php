354b678979d7317988c61c424c480eef8989c203
Azamat9904 <azamatnamialy@gmail.com>
2020-11-10 00:37:28 +0600

    chore(): code was refractored

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,6 @@
 <?php 
     require_once "config/config.php";
-    require_once 'libs/Core.php';
-    require_once 'libs/Controller.php';
-    require_once 'libs/Database.php';
-    
+
+    spl_autoload_register(function($className){
+        require_once 'libs/'.$className . '.php';
+    });
