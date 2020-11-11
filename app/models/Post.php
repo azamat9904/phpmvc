440790c7efd03bb75e545cb7d5d6311034f46719
Azamat9904 <azamatnamialy@gmail.com>
2020-11-11 21:14:24 +0600

    feature(): Displaying posts

--- app/models/Post.php
+++ app/models/Post.php
@@ -7,6 +7,7 @@
         }
 
         public function getPosts(){
-           
+            $this->db->query("SELECT * FROM posts as p INNER JOIN users as u ON p.user_id = u.id ORDER BY p.created_at DESC");
+            return $this->db->resultSet();
         }
     }
--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -7,4 +7,13 @@
             <a href="<?= URLROOT; ?>/posts/add" class="btn btn-primary pull-right"><i class="fa fa-pencil"></i> Add post</a>
         </div>
     </div>
+    <?php foreach($data['posts'] as $post): ?>
+        <div class="card card-body mb-3">
+            <h4 class="card-title"><?= $post->title; ?></h4>
+            <div class="bg-light p-2 mb-3">
+                Written by <?= $post->name ?> on <?= $post->created_at ?>
+            </div>
+            <p class="card-text"><?= $post->body ?></p>
+        </div>
+    <?php endforeach; ?>
 <?php require_once APPROOT . '/views/inc/footer.php'; ?>
